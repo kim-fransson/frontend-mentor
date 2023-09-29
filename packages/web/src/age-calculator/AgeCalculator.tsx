3e4cee23dfb7a2b228c78ef946d73f05886d48e1
Kim Fransson <kim.fransson@sigmatechnology.se>
2023-09-29 15:05:57 +0200

    Age Calculator | added font

--- packages/web/src/age-calculator/AgeCalculator.tsx
+++ packages/web/src/age-calculator/AgeCalculator.tsx
@@ -6,7 +6,7 @@ import { FocusScope } from "react-aria";
 export const AgeCalculator = () => {
   const [birthDate, setBirthDate] = useState<Date>();
   return (
-    <div className="flex flex-col gap-y-14 md:gap-y-6 bg-white px-4 md:px-8 py-10 rounded-t-2xl rounded-bl-2xl rounded-br-[5rem]">
+    <div className="font-poppins flex flex-col gap-y-14 md:gap-y-6 bg-white px-4 md:px-8 py-10 rounded-t-2xl rounded-bl-2xl rounded-br-[5rem]">
       <FocusScope>
         <DateForm onSubmit={setBirthDate} />
       </FocusScope>
