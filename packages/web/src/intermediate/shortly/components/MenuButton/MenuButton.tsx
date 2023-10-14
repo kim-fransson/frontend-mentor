a3e7c11c35f17309fddfe149dfefd8d8384a33ae
Kim Fransson <kim.fransson@sigmatechnology.se>
2023-10-14 02:15:13 +0200

    Shortly | why spend time on animations...

--- packages/web/src/intermediate/shortly/components/MenuButton/MenuButton.tsx
+++ packages/web/src/intermediate/shortly/components/MenuButton/MenuButton.tsx
@@ -5,6 +5,7 @@ import { useRef } from "react";
 import HamburgerMenu from "../../assets/icon-hamburger.svg?react";
 import { Popover } from "../Popover/Popover";
 import { Menu } from "./Menu/Menu";
+import { useIsFirstRender } from "@uidotdev/usehooks";
 
 interface MenuButtonProps<T extends object>
   extends AriaMenuProps<T>,
@@ -12,12 +13,12 @@ interface MenuButtonProps<T extends object>
 
 export const MenuButton = <T extends object>(props: MenuButtonProps<T>) => {
   const state = useMenuTriggerState(props);
+  // got some weird flickering bug for first render
+  const isFirstRender = useIsFirstRender();
 
   const ref = useRef(null);
   const { menuTriggerProps, menuProps } = useMenuTrigger<T>({}, state, ref);
   const { buttonProps } = useButton(menuTriggerProps, ref);
-
-  console.log("rendered");
 
   return (
     <>
@@ -28,7 +29,7 @@ export const MenuButton = <T extends object>(props: MenuButtonProps<T>) => {
       >
         <HamburgerMenu className="w-8 h-8 text-gray-500" />
       </button>
-      {state.isOpen && (
+      {!isFirstRender && (
         <Popover
           state={state}
           triggerRef={ref}
--- packages/web/src/intermediate/shortly/components/Popover/Popover.tsx
+++ packages/web/src/intermediate/shortly/components/Popover/Popover.tsx
@@ -1,3 +1,4 @@
+import { Transition } from "@headlessui/react";
 import { useRef } from "react";
 import { DismissButton, Overlay, usePopover } from "react-aria";
 import type { AriaPopoverProps } from "react-aria";
@@ -26,13 +27,27 @@ export const Popover = ({
   );
 
   return (
-    <Overlay>
-      <div {...underlayProps} className={twMerge("fixed inset-0", className)} />
-      <div {...popoverProps} ref={popoverRef}>
-        <DismissButton onDismiss={state.close} />
-        {children}
-        <DismissButton onDismiss={state.close} />
-      </div>
-    </Overlay>
+    <Transition appear={state.isOpen} show={state.isOpen}>
+      <Overlay>
+        <div
+          {...underlayProps}
+          className={twMerge("fixed inset-0", className)}
+        />
+        <div {...popoverProps} ref={popoverRef}>
+          <DismissButton onDismiss={state.close} />
+          <Transition.Child
+            enter="transform transition duration-300 ease-in-out"
+            enterFrom="opacity-0 -translate-y-10"
+            enterTo="opacity-100 translate-y-0"
+            leave="transform duration-300 transition ease-in-out"
+            leaveFrom="opacity-100 rotate-0 scale-100 "
+            leaveTo="opacity-0 scale-95 "
+          >
+            {children}
+          </Transition.Child>
+          <DismissButton onDismiss={state.close} />
+        </div>
+      </Overlay>
+    </Transition>
   );
 };
