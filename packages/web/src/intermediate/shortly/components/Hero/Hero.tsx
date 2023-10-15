0d8143ac26e3097369471aba05eecdb1928145eb
Kim Fransson <kim.fransson@sigmatechnology.se>
2023-10-15 18:44:17 +0200

    Shortly | enough is enough :D

--- packages/web/src/intermediate/shortly/components/Hero/Hero.tsx
+++ packages/web/src/intermediate/shortly/components/Hero/Hero.tsx
@@ -8,7 +8,7 @@ export const Hero = () => {
     <section
       className={twMerge(
         "px-8 pb-36 flex flex-col overflow-hidden items-center justify-center relative bg-white",
-        "lg:grid lg:grid-cols-2 lg:items-center lg:px-24 lg:pt-8"
+        "lg:grid lg:grid-cols-2 lg:grid-flow-col lg:items-center lg:px-24 lg:pt-8"
       )}
     >
       <div
@@ -23,29 +23,28 @@ export const Hero = () => {
           alt="working lady in front of computer"
         />
       </div>
-      <h2
-        className={twMerge(
-          "text-gray-800 mb-4 font-bold text-4xl tracking-tight text-center",
-          "lg:mb-36 lg:text-6xl lg:text-start lg:col-start-1 lg:col-span-1 lg:row-start-1"
-        )}
-      >
-        More than just shorter links
-      </h2>
-      <p
-        className={twMerge(
-          "font-medium text-gray-400 mb-8 text-center",
-          "lg:col-start-1 lg:col-span-1 lg:row-start-1 lg:mb-10 lg:mt-20 lg:text-start lg:pr-16"
-        )}
-      >
-        Build your brand's recognition and get detailed insights on how your
-        links are performing.
-      </p>
-      <Button
-        size="large"
-        className="lg:col-start-1 lg:col-span-1 lg:row-start-1 lg:mt-52 lg:justify-self-start"
-      >
-        Get Started
-      </Button>
+      <div className="flex flex-col items-center lg:items-start">
+        <h2
+          className={twMerge(
+            "text-gray-800 mb-4 font-bold text-4xl tracking-tight text-center",
+            "lg:text-6xl lg:text-start"
+          )}
+        >
+          More than just shorter links
+        </h2>
+        <p
+          className={twMerge(
+            "font-medium text-gray-400 mb-8 text-center",
+            "lg:text-start lg:pr-16"
+          )}
+        >
+          Build your brand's recognition and get detailed insights on how your
+          links are performing.
+        </p>
+        <Button size="large" className="lg:justify-self-start">
+          Get Started
+        </Button>
+      </div>
     </section>
   );
 };
